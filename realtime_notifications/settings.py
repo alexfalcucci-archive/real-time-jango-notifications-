302e79fb0653233c73cfa9f10b61ef6c57d5759b
alexfalcucc <alex.falcucci@gmail.com>
2015-03-18 20:18:22 -0300

    configuring my settings

--- realtime_notifications/settings.py
+++ realtime_notifications/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+SESSION_ENGINE = 'user_sessions.backends.db'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -36,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rn',
 )
 
 MIDDLEWARE_CLASSES = (
